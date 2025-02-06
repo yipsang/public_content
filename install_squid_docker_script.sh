461a1257732249c042cb91e709bac8dc7cd108a3
Yip Sang Leung <thirtythousandollar@gmail.com>
2025-02-05 23:03:22 -0500

    Update deploy script

--- install_squid_docker_script.sh
+++ install_squid_docker_script.sh
@@ -8,6 +8,11 @@ sudo apt-get install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 
+# Setup Docker permissions
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp - docker
+
 # Pull Squid image and run container
-docker pull sameersbn/squid:latest
-docker run -d -p 3128:3128 --name squid_proxy sameersbn/squid:latest
+docker pull ubuntu/squid:5.2-22.04_beta
+docker run -d --name squid-container -e TZ=UTC -p 3128:3128 ubuntu/squid:5.2-22.04_beta
